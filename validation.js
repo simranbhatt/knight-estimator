f4d5e59ab98c651745a5c7d04a7bd7ac9d655653
Simran <simrankbhatt@gmail.com>
2022-09-18 15:41:33 +0530

    validating user input value

---
+++ validation.js
@@ -0,0 +1,19 @@
+import chessboard from './board.js';
+
+export async function positionIsValid(currentPosition) {
+    if (currentPosition.length === 2) {
+        const row = currentPosition.charAt(0);
+        const column = currentPosition.charAt(1);
+        return isValidRow(row) && isValidColumn(column);
+    }
+    return false;
+}
+
+export function isValidRow(row) {
+    return row > 0 && row < 9;
+}
+
+export function isValidColumn(column) {
+    const columnRange = /^[a-hA-H]+$/
+    return columnRange.test(column);
+}
