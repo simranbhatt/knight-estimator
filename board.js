571657f9d7ff200f358ffe45c25da44c7f5ca7dd
Simran <simrankbhatt@gmail.com>
2022-09-18 15:42:05 +0530

    chess board visualisation on cli

---
+++ board.js
@@ -0,0 +1,15 @@
+export default function chessboard() {
+    const rows = 8;
+    const columns = 8;
+    var board = [];
+    for(var row = 0; row < rows; row++) {
+        board[row] = new Array(columns);
+        console.log("\n");
+        for(var column = 0; column < columns; column++) {
+            var columnChar = String.fromCharCode(97 + column);
+            board[row][column] = row+1 + columnChar;
+            process.stdout.write(board[row][column] + "  ");
+        }
+    }
+    console.log("\n");
+}
